d12a391dede1299cfc0208500938f26caf6e60d7
OlgaVolos <72765623+OlgaVolos@users.noreply.github.com>
2022-01-31 23:33:03 +0200

    hw1

---
+++ lesson1/hw1/js1.js
@@ -0,0 +1,206 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// let gritting = 'hello';
+// let school = 'owu';
+// let company = 'com';
+// let language = 'ua';
+// let number = 1;
+// let a = 10;
+// let b = -999;
+// let c = 123;
+// let p = 3.14;
+// let d = 2.7;
+// let f = 16;
+// let g = true;
+// let k = false;
+
+     // Вивести кожну змінну за допомогою: console.log , alert, document.write
+ // console.log(gritting);
+ // alert(gritting);
+ // document.write(gritting);
+ //
+ // console.log(school);
+ // alert(school);
+ // document.write(school);
+ //
+ // console.log(company);
+ // alert(company);
+ // document.write(company);
+ //
+ // console.log(language);
+ // alert(language);
+ // document.write(language);
+ //
+ // console.log(number);
+ // alert(number);
+ // document.write(number);
+ //
+ // console.log(a);
+ // alert(a);
+ // document.write(a);
+ //
+ // console.log(b);
+ // alert(b);
+ // document.write(b);
+ //
+ // console.log(c);
+ // alert(c);
+ // document.write(c);
+ //
+ // console.log(p);
+ // alert(p);
+ // document.write(p);
+ //
+ // console.log(d);
+ // alert(d);
+ // document.write(d);
+ //
+ // console.log(f);
+ // alert(f);
+ // document.write(f);
+ //
+ // console.log(g);
+ // alert(g);
+ // document.write(g);
+ //
+ // console.log(k);
+ // alert(k);
+ // document.write(k);
+
+ // - Переприсвоїти кожній змінній з завдання значення на довільне.
+//  gritting = 'hi';
+// school = 'okten';
+// company = 'org';
+// language = 'en'
+// number = 2
+// a = 3;
+// b = -5;
+// c = 84;
+// p = 3.56;
+// d = 8.12;
+// f = 25;
+// g = false;
+// k = true;
+
+     // Вивести кожну змінну за допомогою: console.log , alert, document.write
+// console.log(gritting);
+// alert(gritting);
+// document.write(gritting);
+//
+// console.log(school);
+// alert(school);
+// document.write(school);
+//
+// console.log(company);
+// alert(company);
+// document.write(company);
+//
+// console.log(language);
+// alert(language);
+// document.write(language);
+//
+// console.log(number);
+// alert(number);
+// document.write(number);
+//
+// console.log(a);
+// alert(a);
+// document.write(a);
+//
+// console.log(b);
+// alert(b);
+// document.write(b);
+//
+// console.log(c);
+// alert(c);
+// document.write(c);
+//
+// console.log(p);
+// alert(p);
+// document.write(p);
+//
+// console.log(d);
+// alert(d);
+// document.write(d);
+//
+// console.log(f);
+// alert(f);
+// document.write(f);
+//
+// console.log(g);
+// alert(g);
+// document.write(g);
+//
+// console.log(k);
+// alert(k);
+// document.write(k);
+
+//  // - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+// const firstName = 'Volos';
+// const middleName = 'Olga';
+// const lastName = 'Igorivna';
+//
+// const person = firstName + ' ' + middleName + ' ' + lastName;
+// console.log(person);
+
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+ //     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+
+// let name = prompt('Ваше ім"я?');
+// let pobatkovi = prompt("Ваше по-батькові");
+// let age = +prompt("Скільки вам років?");
+// document.write(`"Вітаю, ${name} ${pobatkovi}. Тобі ${age} років."`);
+
+ // - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//      let a = 100;
+//      let b = '100';
+//      let c = true;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+//  - Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+//      В однакових виразах не використовувати однакові оператори!!!
+//      5 ? 6 -> true
+//  5 ? 6 -> false
+//  5 ? 6 -> false
+//  5 ? 6 -> false
+
+//  10 ? 10 -> true
+//  10 ? 10 -> true
+//  10 ? 10 -> false
+//  10 ? 10 -> false
+//  10 ? 10 -> false
+
+//  123 ? '123' -> false
+//  123 ? '123' -> true
+// console.log(5<6);
+// console.log(5>6);
+// console.log(5===6);
+// console.log(5>=6);
+//
+// console.log(10===10);
+// console.log(10==10);
+// console.log(10!==10);
+// console.log(10<10);
+// console.log(10>10);
+//
+// console.log(123 === '123');
+// console.log(123 == '123');
+
+
+
+
+//
+//  - Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
+//      let str = "20";
+//  let a = 5;
+//  document.write(str + a + "<br/>"); 205
+//  document.write(str - a + "<br/>"); 15
+//  document.write(str * "2" + "<br/>"); 40
+//  document.write(str / 2 + "<br/>"); 10
+// */
+
+// document.write(str + a + "<br/>"); 205 - стрінга+число = конкатинує
+//  document.write(str - a + "<br/>"); 15 - решта виконуються арифметичні дій. Число,
+//  document.write(str * "2" + "<br/>"); 40 записане як стрінга, сприймається числом
+//  document.write(str / 2 + "<br/>"); 10
